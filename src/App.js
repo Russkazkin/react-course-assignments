b1dedb25296d3ac30ea51c6904b5cf0c4ce80b32
Russkazkin <ruslan@skazkin.su>
2020-12-10 17:58:03 +0300

    Add assignment structure

--- src/App.js
+++ src/App.js
@@ -11,11 +11,31 @@ function App() {
     const changeMyName = (newName) => setUsernames([newName, 'Sergey']);
     return (
         <div className="App container">
-            <div className="row">
+            <h3 className="mb-3">Assignment 1</h3>
+            <div className="row mb-5">
                 <UserInput changeName={changeMyName} usernames={usernames}/>
                 <UserOutput username={usernames[0]}/>
                 <UserOutput username={usernames[1]}/>
                 <button onClick={showSurnames} className="btn btn-success btn-sm mt-5">Show Surnames</button>
+            </div>
+            <h3 className="mb-3">Assignment 2</h3>
+            <div className="row">
+                <div className="col"><div className="card">
+                    <div className="card-body">
+                        <div className="form-floating mb-3">
+                            <input type="text"
+                                   className="form-control"
+                                   id="floatingInput"
+                                   placeholder="name@example.com"/>
+                            <label htmlFor="floatingInput">Add your letters</label>
+                        </div>
+                    </div>
+                </div></div>
+                <div className="col"><div className="card">
+                    <div className="card-body">
+                        Result
+                    </div>
+                </div></div>
             </div>
         </div>
     );
