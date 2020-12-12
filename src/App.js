43897695977dab0eef1af1ac25b94c7bbcf361f4
Russkazkin <ruslan@skazkin.su>
2020-12-12 09:24:20 +0300

    Add input handler function

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import UserOutput from "./Components/UserOutput/UserOutput";
 
 function App() {
     const [usernames, setUsernames] = useState(['Ruslan', 'Sergey']);
+    const [textInput, setTextInput] = useState(['Here will be your text']);
     const showSurnames = () => setUsernames(['Skazkopodatelev', 'Vechersky']);
     const changeMyName = (newName) => setUsernames([newName, 'Sergey']);
+    const changeInput = (inputText) => setTextInput(inputText);
     return (
         <div className="App container">
             <h3 className="mb-3">Assignment 1</h3>
@@ -26,14 +28,15 @@ function App() {
                             <input type="text"
                                    className="form-control"
                                    id="floatingInput"
+                                   onChange={(event) => changeInput(event.target.value)}
                                    placeholder="name@example.com"/>
-                            <label htmlFor="floatingInput">Add your letters</label>
+                            <label htmlFor="floatingInput">Start to type</label>
                         </div>
                     </div>
                 </div></div>
                 <div className="col"><div className="card">
                     <div className="card-body">
-                        Result
+                        {textInput}
                     </div>
                 </div></div>
             </div>
